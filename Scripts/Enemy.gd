d85b5d893cb210aa559d9b817f54635344fa2314
Kiam11 <kiam.perez@upr.edu>
2023-11-11 17:30:29 -0400

    changes for pokeball

--- Scripts/Enemy.gd
+++ Scripts/Enemy.gd
@@ -1,27 +1,32 @@
-extends Area2D
+extends Node2D
 
 #var where we store
 @export var pokeball_scene = load("res://Scenes/Pokeball.tscn")
-
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
-
-
+"res://Scenes/enemy.tscn"
+var i = 0
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	
-	rotate(0.05)
-	#var pball = pokeball_scene.instantiate()
-	#pball.scale = Vector2(2.5,2.5)
+	#rotate(0.05)
+	var pball = pokeball_scene.instantiate()
 	
-	#pball.position = self.position
+	
+	
+	pball.position = self.position
 	#pball.rotation = self.rotation
-	#pball.position.x += 50
-	#get_parent().add_child(pball)
+	pball.position.x += 10 + i
+	i += 1
+	get_parent().add_child(pball)
 	
+
+
+func _on_timer_timeout():
+	pass # Replace with function body.
