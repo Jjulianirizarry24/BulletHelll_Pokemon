9d8af7d25ea09a222af40a00f61a9197171cb8c3
Jujo <99344130+Jjulianirizarry24@users.noreply.github.com>
2023-11-11 11:11:36 -0400

    Add files via upload

--- Scripts/Pokeball.gd
+++ Scripts/Pokeball.gd
@@ -1,15 +1,24 @@
-extends Node2D
+extends Area2D
 
 #var where we store
 #var pokeball_scene = load("res://Scenes/Pokeball.tscn")
 
+#var vel: Vector2 = Vector2()
+var speed = 100
+var direction = Vector2.RIGHT
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	pass
+
+func _physics_process(delta):
+
+	position += direction * speed * delta
 	#self.position += Vector2(1,0).rotated(self.rotation)
 	
 	#var p = pokeball_scene.instantiate()
@@ -19,3 +28,10 @@ func _process(delta):
 	#p.position.x += 100
 	#get_parent().add_child(p)
 	#self.position.x += 900
+func _on_body_entered(body):
+	if body.is_in_group("player"):
+		body.take_damage(1)
+		queue_free()
+
+func _on_screen_exited():
+	queue_free()
